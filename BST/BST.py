f43d41290a660e3a0fe8dcc3979e756911ddab60
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2023-03-11 10:19:53 -0600

    BST findminvalue() and findmaxvalue().

--- BST/BST.py
+++ BST/BST.py
@@ -5,23 +5,60 @@ class BST:
         self.right = None
 
     def insert(self, data):
-        if data < self.data:
-            if self.left is None:
-                self.left = BST(data)
+        current = self
+        while True:
+            if data < current.data:
+                if current.left is None:
+                    current.left = BST(data)
+                    break
+                else:
+                    current = current.left
             else:
-                self.left.insert(data)
-        elif data > self.data:
-            if self.right is None:
-                self.right = BST(data)
-            else:
-                self.right.insert(data)
+                if data > current.data:
+                    if current.right is None:
+                        current.right = BST(data)
+                        break
+                    else:
+                        current = current.right
+        return self
 
+    def contains(self, data):
+        current = self
+        while current is not None:
+            if data < current.data:
+                current = current.left
+            elif data > current.data:
+                current = current.right
+            else:
+                return True
+        return False
+
+    def findminvalue(self):  # Find left-most node
+        current = self
+        while current.left is not None:
+            current = current.left
+        return current.data
+
+    def findmaxvalue(self):
+        current = self
+        while current.right is not None:
+            current = current.right
+        return current.data
+
+    def preorder(self, root):  # Traversal
+        if root:
+            print(root.data)
+            self.preorder(root.left)
+            self.preorder(root.right)
 
 
 root = BST(27)
-root.insert(27)
 root.insert(35)
 root.insert(10)
 root.insert(19)
 root.insert(31)
 root.insert(42)
+
+# root.preorder(root)
+# root.contains(35)
+print(root.findminvalue())
