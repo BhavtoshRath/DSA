e7d2b3a2c8fc8887cdc187d3d9fb663b1da19d43
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2023-10-13 22:04:29 -0500

    Easy soln for commonStrings.py

--- Strings/commonStrings.py
+++ Strings/commonStrings.py
@@ -1,8 +1,8 @@
 def commonCharacters(strings):
-    char_set = set()
-    for string in strings:
+    char_set = set(strings[0])
+    for string in strings[1:]:
         char_set = char_set.intersection(set(string))
-    return []
+    return list(char_set)
 
 
 strings = ["abc", "bcd", "cbaccd"]
