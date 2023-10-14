f34119016f4be1fa2efa9d96337976e89bdff06d
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2023-10-13 22:45:41 -0500

    2nd soln for commonStrings.py

--- Strings/commonStrings.py
+++ Strings/commonStrings.py
@@ -1,4 +1,4 @@
-def commonCharacters(strings):
+def commonCharacters1(strings):
     char_set = set(strings[0])
     for string in strings[1:]:
         char_set = char_set.intersection(set(string))
@@ -7,4 +7,28 @@ def commonCharacters(strings):
 
 strings = ["abc", "bcd", "cbaccd"]
 
-print(commonCharacters(strings))
+# print(commonCharacters1(strings))
+
+
+# Time: O(N.M) -N - length of strings, M - length of individual string
+# O (C) C- Unique chars across all strings
+# D.S: HashMap
+def commonCharacters2(strings):
+    sol = []
+    d = dict()
+    for string in strings:
+        set_string = set(string)
+        for char in set_string:
+            if char not in d:
+                d[char] = 0
+            d[char] += 1
+    for char in d:
+        if d[char] == len(strings):
+            sol.append(char)
+    return sol
+
+
+strings = ["abc", "bcd", "cbaccd"]
+print(commonCharacters2(strings))
+
+
