e9f845569410b1c380deaa24c50d04d8d7e95195
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2023-10-17 21:56:11 -0500

    Iterative algo for getNthFib.py

--- Recursion/getNthFib.py
+++ Recursion/getNthFib.py
@@ -13,7 +13,7 @@ def getNthFib1(n):
 
 # Using memoization (used to optimize recursive algorithms by storing the results
 # of expensive function calls and reusing them when the same inputs occur again)
-# Time: O(n)
+# Time: O(n) | Space: O(n)
 fib_cache = {}
 fib_cache[1] = 0
 fib_cache[2] = 1
@@ -25,4 +25,26 @@ def getNthFib2(n):
         return fib_cache[n]
 
 
-print(getNthFib2(21))
+# print(getNthFib2(21))
+
+
+# Non-recursive, iterative solution
+# Time: O(n) | Space: O(1)
+def getNthFib3(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    fib_cache = []
+    fib_cache.append(0)
+    fib_cache.append(1)
+    tmp = 2
+    while tmp < n:
+        fib_tmp = fib_cache[0] + fib_cache[1]
+        fib_cache[0] = fib_cache[1]
+        fib_cache[1] = fib_tmp
+        tmp += 1
+    return fib_cache[1]
+
+print(getNthFib3(1))
+# 0,1,1,2,3,5,8,13
