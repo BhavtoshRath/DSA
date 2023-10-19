4e34ea954508fb39cc973911ae75b00407fcbac0
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2023-10-18 21:40:13 -0500

    Completed 2 solns of count_even_digits.py

--- ChatGPT_questions/Recursion/count_even_digits.py
+++ ChatGPT_questions/Recursion/count_even_digits.py
@@ -1,15 +1,37 @@
-# Question:
+# Question: (revisit)
 # Write a recursive function count_even_digits(n) that counts the number of even digits in a positive integer n.
 
 
-# HINT: If need to separate out digits in number, use (% 10), (// 10)
-def count_even_digits(n):
-    m = n % 10
-    if m & 2 == 0:
-        cntr = 1
-    new_n = n // 10
-    count_even_digits(new_n)
-    print(m, new_n)
+# HINT: [An imp concept] If need to separate out digits in number, use (% 10), (// 10)
+# A non-recursive solution
+def count_even_digits1(n):
+    cntr = 0
+    while n != 0:
+        m = n % 10
+        if m & 2 == 0:
+            cntr += 1
+        n = n // 10
+    return cntr
 
 
-count_even_digits(346)
+# print(count_even_digits1(3478568887))
+
+# Can recursion have a while loop?
+# Base case: single digit
+cntr = 0
+def count_even_digits2(n):
+    last_digit = n % 10
+    remaining_digit = n // 10
+    if n < 10:
+        if n % 2 == 0:
+            return 1
+        else:
+            return 0
+    else:
+        return (last_digit % 2 == 0) + count_even_digits2(remaining_digit)  # imp way to assign "counter in recursion"
+
+
+# how does (last_digit % 2 == 0), a boolean serve as counter?
+
+print(count_even_digits2(349885))
+
