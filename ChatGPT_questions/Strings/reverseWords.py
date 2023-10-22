9476dfcd2a3d4c1898353ea94439b165f50cd05b
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2023-10-21 19:01:40 -0500

    Added reverseWords.py

---
+++ ChatGPT_questions/Strings/reverseWords.py
@@ -0,0 +1,12 @@
+# You are given a string containing a series of words separated by spaces.
+# Write a function to reverse the order of the words in the string.
+# For example, given the input string "Hello World OpenAI",
+# your function should return "OpenAI World Hello".
+
+def reverseWords(str):
+    splt_str = str.split(' ')
+    res = " ".join(reversed(splt_str))
+    return res
+
+
+print(reverseWords("Hello world"))
