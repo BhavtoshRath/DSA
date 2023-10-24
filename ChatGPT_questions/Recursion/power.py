42f37137bccfb4e9b791702048cb4d8b3fc27ac2
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2023-10-23 19:28:11 -0500

    Added power.py for case when n < 0

--- ChatGPT_questions/Recursion/power.py
+++ ChatGPT_questions/Recursion/power.py
@@ -5,8 +5,14 @@ def power(x, n):
         return 1
     if n == 1:
         return x
+    if n < 0:
+        return 1/x * power(x, n + 1)
     else:
         return x * power(x, n-1)
 
 
-print(power(5, 7))
+print(power(3, -3))
+
+# assert power(2, 3) == 8
+# assert power(5, 0) == 1
+# assert power(3, -2) == 1/9
