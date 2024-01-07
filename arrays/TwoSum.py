948a15b4db0a25186b700c82059990942469fd23
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2024-01-07 10:19:01 -0600

    twoSum docstringss added

--- arrays/TwoSum.py
+++ arrays/TwoSum.py
@@ -1,27 +1,29 @@
+# Given an array of integers nums and an integer targetSum,
+# return indices of the two numbers such that they add up to targetSum.
 
 
-def twoNumberSum1(array, targetSum):
+def twoNumberSum1(array, targetSum):  # Approach 1: brute force (O(N^2))
     l = []
     for i in range(len(array)):
-        for j in range(i+1, len(array)):
+        for j in range(i + 1, len(array)):
             if array[i] + array[j] == targetSum:
                 l.append(i)
                 l.append(j)
     return l
 
 
-def twoNumberSum2(array, targetSum):
+def twoNumberSum2(array, targetSum):  # Approach 2: Hash map/dict (O(N))
     d = {}
     for index, value in enumerate(array):
         d[value] = index
         key_set = set(d.keys())
-        if targetSum-value in key_set and targetSum != 2*value:
-            return [value, targetSum-value]
+        if targetSum - value in key_set and targetSum != 2 * value:
+            return [value, targetSum - value]
     else:
         return []
 
 
-def twoNumberSum3(array, targetSum):
+def twoNumberSum3(array, targetSum):  # Approach 3: Using pointers (O(N))
     sorted_array = sorted(array)
     left_ptr = 0
     right_ptr = len(sorted_array) - 1
