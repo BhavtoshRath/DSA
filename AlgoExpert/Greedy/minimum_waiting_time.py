58adf9b8ac40c76a8dd1444c5735778b1a244c92
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2024-01-18 09:14:36 -0600

    Minor changes to minimum_waiting_time.py

--- AlgoExpert/Greedy/minimum_waiting_time.py
+++ AlgoExpert/Greedy/minimum_waiting_time.py
@@ -2,8 +2,7 @@ def minimumWaitingTime(queries):
     sorted_queries = sorted(queries)
     ans = 0
     for idx in range(1, len(sorted_queries)):
-        sub_queries = sorted_queries[0 : idx + 1]
-        ans += sum(sub_queries[:-1])
+        ans += sum(sorted_queries[0:idx])
     return ans
 
 
