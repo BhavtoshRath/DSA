7e3fa59c5398a4fcbff79b6beb43ad81ea7c7753
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2024-02-02 10:42:34 -0600

    Solve ransomNote using hash map

---
+++ Grind75/ransomNote.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/ransom-note/description/
+# Given two strings ransomNote and magazine, return true if
+# ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+def canConstruct(ransomNote, magazine):  # Use Hash map
+    magazine_dict = {}
+    for char in magazine:
+        if char in magazine_dict:
+            magazine_dict[char] += 1
+        else:
+            magazine_dict[char] = 1
+
+    for char in ransomNote:
+        if char in magazine_dict:
+            magazine_dict[char] -= 1
+        else:
+            return False
+
+    if min(magazine_dict.values()) < 0:
+        return False
+    else:
+        return True
+
+print(canConstruct("aa", "aab"))
