f49c815ba17e23b9eed4ef5f3403a34bcb6c3e73
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2024-02-11 23:11:40 -0600

    Boyer-Moore Voting Algorithm soln for majority element, sso space complexity is O(1)

--- Grind75/MajorityElement.py
+++ Grind75/MajorityElement.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/majority-element/description/
 
-def majorityElement(nums):  # Time C O(N), Space: O(N)
+
+def majorityElement1(nums):  # Time C O(N), Space: O(N)
     maj_dict = {}
     for num in nums:
         if num in maj_dict:
@@ -9,4 +10,20 @@ def majorityElement(nums):  # Time C O(N), Space: O(N)
             maj_dict[num] = 1
     return max(maj_dict, key=maj_dict.get)
 
-print(majorityElement([1,2,3,4,3,3,2,3]))
+
+print(majorityElement1([1, 2, 3, 4, 3, 3, 2, 3]))
+
+
+def majorityElement2(nums):  # Time C O(N), Space: O(N) [Boyer-Moore Voting Algorithm]
+    # Works only when majority element exists alteast n/2 times.
+    candidate = None
+    count = 0
+
+    for num in nums:
+        if count == 0:
+            candidate = num
+            count = 1
+        elif num == candidate:
+            count += 1
+        else:
+            count -= 1
