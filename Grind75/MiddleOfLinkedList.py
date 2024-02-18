0502f0a1b8cc68c4ab71dff8e59dd7bc8bbd8749
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2024-02-17 19:39:18 -0600

    Soln for middle of linked list

---
+++ Grind75/MiddleOfLinkedList.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/middle-of-the-linked-list/description/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def MiddleNode(head):
+    if not head:
+        return None
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+
+
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+head.next.next.next.next = ListNode(5)
+
+middle_node = find_middle(head)
+if middle_node:
+    print("Middle of the linked list:", middle_node.val)
+else:
+    print("Linked list is empty.")
