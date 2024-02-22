41e516e3a441c63ec54b83b383cee49dfd681c0f
BhavtoshRath <bhavtoshrath.umn@gmail.com>
2024-02-21 19:35:53 -0600

    Flood Fill (dfs on graph/matrix)

--- Grind75/FloodFill.py
+++ Grind75/FloodFill.py
@@ -1,4 +1,4 @@
-
+# https://leetcode.com/problems/flood-fill/description/
 
 
 def flood_fill(image, sr, sc, new_color):   # dfs on graph
@@ -6,9 +6,11 @@ def flood_fill(image, sr, sc, new_color):   # dfs on graph
         return image
 
     def dfs(row, col, old_color):
-        print(row, col)
-        if row < 0 or row > len(image) or col < 0 or col > len(image[0]) or image[row][col] != old_color:
-            return
+        # if image[row][col] != old_color:
+        #     print(row, col)
+        if row < 0 or row > len(image) or col < 0 or col > len(image[0]) or image[row][col] != old_color: # Not sure about image[row][col] != old_color
+            print(row, col)
+            return                  # imp concept
         image[row][col] = new_color
         dfs(row - 1, col, old_color)
         dfs(row + 1, col, old_color)
